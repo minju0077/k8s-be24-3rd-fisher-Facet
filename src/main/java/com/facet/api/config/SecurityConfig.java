6f33816ec91f7cc20665749a331c9a7bda881cf1
dwg245 <dwg0245@gmail.com>
2026-03-03 16:58:51 +0900

    [feat] 카카오톡 소셜 로그인시 쿠키에 Session 사용 안하게 설정

--- src/main/java/com/facet/api/config/SecurityConfig.java
+++ src/main/java/com/facet/api/config/SecurityConfig.java
@@ -22,6 +22,7 @@ package com.facet.api.config;
 
 import com.facet.api.config.filter.JwtFilter;
 import com.facet.api.config.oauth2.OAuth2AuthenticationSuccessHandler;
+import com.facet.api.config.oauth2.OAuth2AuthorizationRequestRepository;
 import com.facet.api.user.OAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +32,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,6 +45,7 @@ public class SecurityConfig {
     private final JwtFilter jwtFilter;
     private final OAuth2UserService oAuth2UserService;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthorizationRequestRepository oAuth2AuthorizationRequestRepository;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -57,7 +60,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
         http.oauth2Login(config -> {
+            config.authorizationEndpoint(endpoint ->
+                    endpoint.authorizationRequestRepository(oAuth2AuthorizationRequestRepository)
+            );
+
             config.userInfoEndpoint(
                     endpoint ->
                             endpoint.userService(oAuth2UserService)
