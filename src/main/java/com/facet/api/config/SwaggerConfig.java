7a6c5fc39afc7e7ad2fdfa1ef8e7444f8429db22
mj <qjsalswn@gmail.com>
2026-03-18 14:42:27 +0900

    [Docs] Swagger 상세 사용법 가이드 작성
    - @Tag, @Operation, @Schema 사용법과 설명 기재

--- src/main/java/com/facet/api/config/SwaggerConfig.java
+++ src/main/java/com/facet/api/config/SwaggerConfig.java
@@ -16,4 +16,27 @@ public class SwaggerConfig {
                         .version("1.0.0")
                 );
     }
+    /*
+        Swagger 사용법
+
+        - 구현한 기능에 @Tag 를 붙여서 제목을 넣을 수 있다. (주로 controller에 사용한다.)
+        ex)
+            @Tag(name="경매 기능")
+            public class AuctionController{...}
+
+        - 메소드에 @Operation 를 붙여서 실제 구현된 기능들에 이름과 상세 설명을을 붙일 수 있다.
+        ex)
+            @Operation(summary = "입찰 기능", description = "현재 입찰가보다 높은 금액을 입력하면 입찰할 수 있는 기능")
+            public ResponseEntity bid(AuctionDto.BidReq dto){...}
+
+        - DTO에 선언된 변수에 @Schema를 붙여서 입력할 데이터 형식의 설명이나 제약 조건, 예시 등을 넣을 수 있다.
+          (required = true로 설정하면 필수로 입력할 수 있도록 설정할 수 있다.)
+        ex)
+            public static class BidReq{
+                private Long productIdx;
+
+                @Schema(description = "입찰가는 현재 입찰가보다 높은 금액을 넣어야 합니다.", required = true, example = "50000")
+                private Long bid;
+            }
+    */
 }
