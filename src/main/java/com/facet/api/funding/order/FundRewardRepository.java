6e2f82e08e49d66ca1a1d131e033fc6f356c39b2
dwg245 <dwg0245@gmail.com>
2026-03-22 23:35:42 +0900

    [Feat] 펀딩 리워드 조회 비관적 락 기능 구현 리워드 리스트 조회 이후 재고 차감 부분에 비관적 락을 이용한 동시성 제어

--- src/main/java/com/facet/api/funding/order/FundRewardRepository.java
+++ src/main/java/com/facet/api/funding/order/FundRewardRepository.java
@@ -2,9 +2,19 @@ package com.facet.api.funding.order;
 
 import com.facet.api.funding.model.FundRewards;
 import com.facet.api.funding.order.model.FundOrdersDto;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FundRewardRepository extends JpaRepository<FundRewards,Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
+    @Query("select r from FundRewards r where r.idx in :ids")
+    List<FundRewards> findAllByIdWithPessimisticLock(@Param("ids") List<Long> ids);
 }
