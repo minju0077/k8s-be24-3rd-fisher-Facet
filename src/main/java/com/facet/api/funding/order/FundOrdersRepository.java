d8b25218558839c3f3c036456d4c472d777b9a76
sarapoba <lhk981112@gmail.com>
2026-03-23 03:59:42 +0900

    [Feat] 주문 참여 내역 기능 추가
    아직 펀딩 데이터만 뽑아옴

--- src/main/java/com/facet/api/funding/order/FundOrdersRepository.java
+++ src/main/java/com/facet/api/funding/order/FundOrdersRepository.java
@@ -3,5 +3,8 @@ package com.facet.api.funding.order;
 import com.facet.api.funding.order.model.FundOrders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FundOrdersRepository extends JpaRepository<FundOrders,Long> {
+    List<FundOrders> findAllByOrdersIdx(Long ordersIdx);
 }
