957a981fa98705e546e347143c586139e2141adc
mj <qjsalswn@gmail.com>
2026-03-24 15:34:12 +0900

    [Chore] 배포용 설정
    cors에러, 포트번호 중복 문제 해결

--- src/main/java/com/facet/api/config/SecurityConfig.java
+++ src/main/java/com/facet/api/config/SecurityConfig.java
@@ -111,7 +111,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("https://www.facet7.kro.kr", "https://facet7.kro.kr"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
