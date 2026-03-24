7d5f7693deb47985bd6bcafe1b7acee0840221d4
dwg245 <dwg0245@gmail.com>
2026-03-24 14:39:11 +0900

    [Fix] swagger 추가 수정

--- src/main/java/com/facet/api/auction/controller/AuctionReadController.java
+++ src/main/java/com/facet/api/auction/controller/AuctionReadController.java
@@ -3,6 +3,8 @@ package com.facet.api.auction.controller;
 import com.facet.api.auction.service.AuctionReadService;
 import com.facet.api.auction.model.AucDto;
 import com.facet.api.common.model.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auction")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "경매 상품 리스트 조회 기능")
 public class AuctionReadController {
     private final AuctionReadService auctionReadService;
 
     @GetMapping("/mainList")
+    @Operation(summary = "경매 메인 페이지 리스트 조회 (페이징) 기능", description = "메인 화면에 노출될 기본적인 경매 상품 목록을 조회.")
     private ResponseEntity mainList(
             @RequestParam(required = true, defaultValue = "0") int page,
             @RequestParam(required = true, defaultValue = "10") int size
@@ -23,6 +27,7 @@ public class AuctionReadController {
         return ResponseEntity.ok(BaseResponse.success(dto)  );
     }
     @GetMapping("/list")
+    @Operation(summary = "경매 전체 리스트 목록 조회 (페이징) 기능", description = "메인 화면에 노출될 기본적인 경매 상품 목록을 조회.")
     private ResponseEntity list(
             @RequestParam(required = true, defaultValue = "0") int page,
             @RequestParam(required = true, defaultValue = "12") int size
@@ -33,12 +38,15 @@ public class AuctionReadController {
     }
 
     @GetMapping("/search/{search}")
+    @Operation(summary = "경매 상품 검색 기능", description = "경매 상품을 검색하여 해당하는 상품 정보 조회")
     public ResponseEntity search(@PathVariable String search){
         AucDto.PageRes dto = auctionReadService.search(0, 12, search);
         return ResponseEntity.ok(dto);
     }
 
+
     @GetMapping("/detail/{prodIdx}")
+    @Operation(summary = "경매 상품 상세 정보 조회 기능", description = "특정 경매 상품(prodIdx) 상세 정보 조회.")
     public ResponseEntity detail(@PathVariable Long prodIdx){
         AucDto.DetailRes dto = auctionReadService.detail(prodIdx);
         return ResponseEntity.ok(BaseResponse.success(dto));
--- src/main/java/com/facet/api/auction/controller/UploadController.java
+++ src/main/java/com/facet/api/auction/controller/UploadController.java
@@ -3,6 +3,8 @@ package com.facet.api.auction.controller;
 import com.facet.api.auction.model.AucDto;
 import com.facet.api.auction.service.S3ImageService;
 import com.facet.api.common.model.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,16 +15,19 @@ import java.util.List;
 @RequestMapping("/auction/image")
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "이미지 업로드 및 조회 기능")
 public class UploadController {
     private final S3ImageService s3ImageService;
 
     @GetMapping("/upload")
+    @Operation(summary = "경매 상품 이미지 S3 저장 ", description = "경매 상품 다중 이미지 S3에 저장 및 DB에 저장 ")
     public ResponseEntity upload(AucDto.ImageReq dto){
         List<String> result = s3ImageService.upload(dto);
         return ResponseEntity.ok(BaseResponse.success(result));
     }
 
     @GetMapping("/get/{productIdx}")
+    @Operation(summary = "경매 상품 이미지 조회 기능 (S3)", description = "특정 경매 상품(prodIdx) 이미지 정보 조회.")
     public ResponseEntity getImage(@PathVariable Long productIdx){
         String imagePath = s3ImageService.getImage(productIdx);
         return ResponseEntity.ok(imagePath);
