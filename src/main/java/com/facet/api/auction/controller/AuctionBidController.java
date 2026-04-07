c03a7554df16580545d7fc6bc5756d9c169db8e6
mj <qjsalswn@gmail.com>
2026-04-07 16:09:06 +0900

    [Feat] v9 젠킨스 배포 테스트 젠킨스파일 방법으로 해보기 코드 변경

--- src/main/java/com/facet/api/auction/controller/AuctionBidController.java
+++ src/main/java/com/facet/api/auction/controller/AuctionBidController.java
@@ -44,11 +44,11 @@ public class AuctionBidController {
 
     @PostMapping("/success")
     public ResponseEntity success(){
-        return ResponseEntity.ok("성공");
+        return ResponseEntity.ok("성공1");
     }
 
     @PostMapping("/fail")
     public ResponseEntity fail(){
-        return ResponseEntity.ok("실패");
+        return ResponseEntity.ok("실패1");
     }
 }
